f080cb447bc8f17eea14ceb0eb2227804ad9d23b
gayathiri1705 <gayupapathi17@gmail.com m>
2026-02-27 23:43:52 +0530

    ac

--- server.js
+++ server.js
@@ -32,3 +32,6 @@ app.get('/health', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// Export the Express API for Vercel
+module.exports = app;
